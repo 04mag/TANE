ce07919d76ccdf36889a176486b5386bb006bf3d
04mag <mathiasgreisen@gmail.com>
2025-04-23 12:40:21 +0200

    Tilføjede interfaces og klasser for Dage

--- TANE.Application/Groups/Dage/Commands/Interfaces/ICreateDag.cs
+++ TANE.Application/Groups/Dage/Commands/Interfaces/ICreateDag.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TANE.Domain.Entities;
 
 namespace TANE.Application.Groups.Dage.Commands.Interfaces
 {
     internal interface ICreateDag
     {
+        Task<Dag> CreateDagAsync(Dag dag);
     }
 }
--- TANE.Application/Groups/Dage/Queries/Interfaces/IReadDag.cs
+++ TANE.Application/Groups/Dage/Queries/Interfaces/IReadDag.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TANE.Domain.Entities;
 
 namespace TANE.Application.Groups.Dage.Queries.Interfaces
 {
     internal interface IReadDag
     {
+        Task<Dag> ReadDagAsync(int id);
+        Task<List<Dag>> ReadDageAsync();
     }
 }
