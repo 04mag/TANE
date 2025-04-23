988ebd1af9ce521b3844be7d34a4f88cf468e15e
04mag <mathiasgreisen@gmail.com>
2025-04-23 12:41:25 +0200

    Tilføjede interfaces og klasser for RejsePlanSkabeloner

---
+++ TANE.Application/Groups/RejsePlanSkabeloner/Commands/CreateRejsePlanSkabelon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TANE.Application.Groups.RejsePlanSkabeloner.Commands.Interfaces;
+using TANE.Application.RepositoryInterfaces;
+using TANE.Domain.Entities;
+
+namespace TANE.Application.Groups.RejsePlanSkabeloner.Commands
+{
+    public class CreateRejsePlanSkabelon : ICreateRejsePlanSkabelon
+    {
+        private readonly IRejsePlanSkabelonRepository _rejsePlanSkabelonRepository;
+
+        public CreateRejsePlanSkabelon(IRejsePlanSkabelonRepository rejsePlanSkabelonRepository)
+        {
+            _rejsePlanSkabelonRepository = rejsePlanSkabelonRepository;
+        }
+
+        public async Task<RejsePlanSkabelon> CreateRejsePlanSkabelonAsync(RejsePlanSkabelon rejsePlanSkabelon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TANE.Application/Groups/RejsePlanSkabeloner/Commands/DeleteRejsePlanSkabelon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TANE.Application.Groups.RejsePlanSkabeloner.Commands.Interfaces;
+using TANE.Application.RepositoryInterfaces;
+
+namespace TANE.Application.Groups.RejsePlanSkabeloner.Commands
+{
+    public class DeleteRejsePlanSkabelon : IDeleteRejsePlanSkabelon
+    {
+        private readonly IRejsePlanSkabelonRepository _rejsePlanSkabelonRepository;
+
+        public DeleteRejsePlanSkabelon(IRejsePlanSkabelonRepository rejsePlanSkabelonRepository)
+        {
+            _rejsePlanSkabelonRepository = rejsePlanSkabelonRepository;
+        }
+
+        public async Task<bool> DeleteRejsePlanSkabelonAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
