afcd5021f0464072118c3644cf3f447b8b0b743f
04mag <mathiasgreisen@gmail.com>
2025-04-23 10:26:31 +0200

    Tilføjede ApplicationDependencyInjection til registrering af dependencies

---
+++ TANE.Application/Configuration/ApplicationDependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TANE.Application.Configuration
+{
+    public static class ApplicationDependencyInjection
+    {
+        public static IServiceCollection RegisterApplicationLayer(this IServiceCollection services)
+        {
+            //Registrer application services her
+
+            return services;
+        }
+    }
+}
