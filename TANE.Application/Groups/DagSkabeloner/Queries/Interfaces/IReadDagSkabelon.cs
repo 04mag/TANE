dc6a39c73a3f2d3a2f11194d0f37462a6a0b3111
04mag <mathiasgreisen@gmail.com>
2025-04-23 10:55:18 +0200

    Har tilføjet tomme interfaces til commands og queries for alle grupper

---
+++ TANE.Application/Groups/DagSkabeloner/Queries/Interfaces/IReadDagSkabelon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TANE.Application.Groups.DagSkabeloner.Queries.Interfaces
+{
+    internal interface IReadDagSkabelon
+    {
+    }
+}
---
+++ TANE.Application/Groups/RejsePlanSkabeloner/Queries/Interfaces/IReadRejsePlanSkabelon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TANE.Application.Groups.RejsePlanSkabeloner.Queries.Interfaces
+{
+    internal interface IReadRejsePlanSkabelon
+    {
+    }
+}
