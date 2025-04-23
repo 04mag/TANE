e1e27593c91d3f42896db9336a7af37aaf6f7503
04mag <mathiasgreisen@gmail.com>
2025-04-23 12:41:03 +0200

    Tilføjede interfaces og klasser for JwtTokens

---
+++ TANE.Application/Groups/JwtTokens/Commands/Interfaces/IRefreshToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TANE.Application.Groups.JwtTokens.Commands.Interfaces
+{
+    internal interface IRefreshToken
+    {
+        Task<string> RefreshTokenAsync(string token, string refreshToken);
+    }
+}
---
+++ TANE.Application/Groups/JwtTokens/Commands/RefreshToken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TANE.Application.Groups.JwtTokens.Commands.Interfaces;
+using TANE.Application.RepositoryInterfaces;
+
+namespace TANE.Application.Groups.JwtTokens.Commands
+{
+    public class RefreshToken : IRefreshToken
+    {
+        private readonly IJwtTokenRepository _jwtTokenRepository;
+
+        public RefreshToken(IJwtTokenRepository jwtTokenRepository)
+        {
+            _jwtTokenRepository = jwtTokenRepository;
+        }
+        public async Task<string> RefreshTokenAsync(string token, string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
