886759e80da62536f456ca978b1cc6a983a39836
04mag <mathiasgreisen@gmail.com>
2025-04-23 11:55:41 +0200

    Har tilføjet klasser og interfaces for kunder til queries og commands

---
+++ TANE.Application/Groups/Kunder/Queries/ReadKunde.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TANE.Application.Groups.Kunder.Queries.Interfaces;
+using TANE.Application.RepositoryInterfaces;
+using TANE.Domain.Entities;
+
+namespace TANE.Application.Groups.Kunder.Queries
+{
+    public class ReadKunde : IReadKunde
+    {
+        private readonly IKundeRepository _kundeRepository;
+
+        public ReadKunde(IKundeRepository kundeRepository)
+        {
+            _kundeRepository = kundeRepository;
+        }
+
+        public async Task<List<Kunde>> GetAllKunderAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Kunde> GetKundeByIdAsync(int kundeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
