e91740e48687938df7ae520280652c239d7d376e
04mag <mathiasgreisen@gmail.com>
2025-04-24 08:57:34 +0200

    Added start af CreateKundeTest

---
+++ TANE.Application.Test/Groups/Kunder/CreateKundeTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TANE.Domain.Entities;
+
+namespace TANE.Application.Test.Groups.Kunder
+{
+    public class CreateKundeTest
+    {
+        [Fact]
+        public void CreateKunde_ValidInput_CreatesKunde()
+        {
+            //// Arrange
+            //var kunde = new Kunde
+            //{
+            //    Id = 1,
+            //    Name = "Test Kunde",
+            //    Adresse = "Test Adresse"
+            //};
+            //// Act
+            //var result = CreateKunde(kunde);
+            //// Assert
+            //Assert.NotNull(result);
+            //Assert.Equal(kunde.Name, result.Name);
+            //Assert.Equal(kunde.Adresse, result.Adresse);
+        }
+    }
+}
