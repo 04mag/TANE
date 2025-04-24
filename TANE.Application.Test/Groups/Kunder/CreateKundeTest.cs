a517c697baeb0918cff06be3ac1a22a919786204
04mag <mathiasgreisen@gmail.com>
2025-04-24 09:59:04 +0200

    Added CreateKunde_ValidInput test

--- TANE.Application.Test/Groups/Kunder/CreateKundeTest.cs
+++ TANE.Application.Test/Groups/Kunder/CreateKundeTest.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TANE.Application.Groups.Kunder.Commands;
+using TANE.Application.RepositoryInterfaces;
 using TANE.Domain.Entities;
 
 namespace TANE.Application.Test.Groups.Kunder
@@ -10,21 +13,27 @@ namespace TANE.Application.Test.Groups.Kunder
     public class CreateKundeTest
     {
         [Fact]
-        public void CreateKunde_ValidInput_CreatesKunde()
+        public async Task CreateKunde_ValidInput()
         {
-            //// Arrange
-            //var kunde = new Kunde
-            //{
-            //    Id = 1,
-            //    Name = "Test Kunde",
-            //    Adresse = "Test Adresse"
-            //};
-            //// Act
-            //var result = CreateKunde(kunde);
-            //// Assert
-            //Assert.NotNull(result);
-            //Assert.Equal(kunde.Name, result.Name);
-            //Assert.Equal(kunde.Adresse, result.Adresse);
+            // Arrange
+            //Create mock of IKundeRepository
+            var kundeRepository = new Mock<IKundeRepository>();
+
+            // Setup the mock to return a Kunde object when CreateKundeAsync is called
+            kundeRepository.Setup(repo => repo.CreateKundeAsync(It.IsAny<Kunde>()))
+                .ReturnsAsync((Kunde kunde) => kunde);
+
+            CreateKunde createKundeCommand = new CreateKunde(kundeRepository.Object);
+
+            var kunde = new Kunde("TestFornavn", "TestEfternavn", "TestEmail@mail.dk", "TestTlfNummer");
+
+            // Act
+            var result = await createKundeCommand.CreateKundeAsync(kunde);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(kunde.Fornavn, result.Fornavn);
+            Assert.Equal(kunde.Email, result.Email);
         }
     }
 }
