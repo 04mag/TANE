506ac97460397303e53bb1dd1fc1f9f62fe4346b
04mag <mathiasgreisen@gmail.com>
2025-04-24 11:41:02 +0200

    Added persistence dependency injection

---
+++ TANE.Persistence/Configuration/PersistenceDependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TANE.Application.RepositoryInterfaces;
+using TANE.Persistence.Repositories;
+
+namespace TANE.Persistence.Configuration
+{
+    public static class PersistenceDependencyInjection
+    {
+        public static IServiceCollection RegisterPersistenceLayer(this IServiceCollection services)
+        {
+            // Add your persistence-related services here
+
+            //Repositories
+            services.AddScoped<IJwtTokenRepository, JwtTokenRepository>();
+
+            return services;
+        }
+    }
+}
