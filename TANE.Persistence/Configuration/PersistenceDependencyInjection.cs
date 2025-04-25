83e7f9e2b93cd9e0e7ae80dc74336ce0a7315d85
04mag <mathiasgreisen@gmail.com>
2025-04-25 12:58:45 +0200

    Added http client dependency injection

--- TANE.Persistence/Configuration/PersistenceDependencyInjection.cs
+++ TANE.Persistence/Configuration/PersistenceDependencyInjection.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using TANE.Application.RepositoryInterfaces;
@@ -13,7 +14,16 @@ namespace TANE.Persistence.Configuration
     {
         public static IServiceCollection RegisterPersistenceLayer(this IServiceCollection services)
         {
+            //Set base adress (Yarp adress) for http clients
+            var baseAddress = new Uri("https://localhost:7100/"); // Adjust the base address as needed
+
             // Add your persistence-related services here
+
+            //Add http clients
+            services.AddHttpClient<IJwtTokenRepository, JwtTokenRepository>(client =>
+            {
+                client.BaseAddress = new Uri($"{baseAddress}"); // Adjust the base address when yarp is added (add "auth/")
+            });
 
             //Repositories
             services.AddScoped<IJwtTokenRepository, JwtTokenRepository>();
