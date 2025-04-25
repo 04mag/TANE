37d583dc6cb0548fef80098ea6a680f7211b34ec
04mag <mathiasgreisen@gmail.com>
2025-04-25 14:23:28 +0200

    Implemented LoginAsync

--- TANE.Application/Groups/JwtTokens/Commands/UserLogin.cs
+++ TANE.Application/Groups/JwtTokens/Commands/UserLogin.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TANE.Application.Common.Exceptions;
 using TANE.Application.Groups.JwtTokens.Commands.Interfaces;
 using TANE.Application.RepositoryInterfaces;
 using TANE.Domain.Entities;
@@ -20,7 +21,19 @@ namespace TANE.Application.Groups.JwtTokens.Commands
 
         public async Task<JwtToken> LoginAsync(string email, string password)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var jwtToken = await _jwtTokenRepository.UserLogin(email, password);
+                return jwtToken;
+            }
+            catch (NotAuthorizedException ex)
+            {
+                throw new UnauthorizedAccessException(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while logging in", ex);
+            }
         }
     }
 }
