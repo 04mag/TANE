d031a879b6b059acef5ff700b642c0c8b067fb65
04mag <mathiasgreisen@gmail.com>
2025-04-29 16:22:59 +0200

    Change to public to inject in ui

--- TANE.Application/Groups/DagSkabeloner/Queries/Interfaces/IReadDagSkabelon.cs
+++ TANE.Application/Groups/DagSkabeloner/Queries/Interfaces/IReadDagSkabelon.cs
@@ -7,7 +7,7 @@ using TANE.Domain.Entities;
 
 namespace TANE.Application.Groups.DagSkabeloner.Queries.Interfaces
 {
-    internal interface IReadDagSkabelon
+    public interface IReadDagSkabelon
     {
         Task<DagSkabelon> ReadDagSkabelonByIdAsync(int id, string jwtToken);
         Task<List<DagSkabelon>> ReadAllDagSkabelonerAsync(string jwtToken);
