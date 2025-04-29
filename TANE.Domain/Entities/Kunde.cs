7f4aafb911bbdb269a4f749c8d2387173f192194
nikolaj <nikolajfreeland@gmail.com>
2025-04-29 13:45:12 +0200

    opdateret kunde entitiy sat rowversion på og added dataannotations

--- TANE.Domain/Entities/Kunde.cs
+++ TANE.Domain/Entities/Kunde.cs
@@ -1,18 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+
 
 namespace TANE.Domain.Entities
 {
     public class Kunde
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Fornavn er påkrævet")]
+        [StringLength(50, ErrorMessage = "Fornavn må ikke være længere end 50 tegn")]
         public string Fornavn { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Efternavn er påkrævet")]
+        [StringLength(50, ErrorMessage = "Efternavn må ikke være længere end 50 tegn")]
         public string Efternavn { get; set; } = string.Empty;
+        
+        [EmailAddress(ErrorMessage = "Email er ikke valid")]
         public string Email { get; set; } = string.Empty;
+        
+        [StringLength(20, ErrorMessage = "Telefonnummer må ikke være længere end 20 tegn")]
         public string TlfNummer { get; set; } = string.Empty;
+
+        public byte[] RowVersion { get; set; } = Array.Empty<byte>();
+
 
         public Kunde()
         {
