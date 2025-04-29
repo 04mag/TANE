6395cc8dd9a03754b75e28547d8621e5fa55667a
nikolaj <nikolajfreeland@gmail.com>
2025-04-29 09:23:32 +0200

    lavet all test skulle gerne virke dog en update test hvor der er noget fejl der ikke var der igør

--- TANE.Application/Groups/Kunder/Queries/ReadKunde.cs
+++ TANE.Application/Groups/Kunder/Queries/ReadKunde.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TANE.Application.Groups.Kunder.Queries.Interfaces;
+﻿using TANE.Application.Groups.Kunder.Queries.Interfaces;
 using TANE.Application.RepositoryInterfaces;
 using TANE.Domain.Entities;
 
