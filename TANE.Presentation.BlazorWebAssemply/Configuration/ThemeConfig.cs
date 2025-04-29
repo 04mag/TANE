f4892e6d78fb7b7c7eb1adfffc82e5aff62433ae
04mag <mathiasgreisen@gmail.com>
2025-04-29 13:41:33 +0200

    Added config for form theme

---
+++ TANE.Presentation.BlazorWebAssemply/Configuration/ThemeConfig.cs
@@ -0,0 +1,9 @@
+﻿using Radzen;
+
+namespace TANE.Presentation.BlazorWebAssemply.Configuration
+{
+    public static class ThemeConfig
+    {
+        public static Variant FormVariant = Variant.Flat;
+    }
+}
