7016da0216431cb5fc9f7b1837ef89504a96a44f
04mag <mathiasgreisen@gmail.com>
2025-04-29 16:22:37 +0200

    Added dependency for commands and queries of dagskabelon

--- TANE.Application/Configuration/ApplicationDependencyInjection.cs
+++ TANE.Application/Configuration/ApplicationDependencyInjection.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TANE.Application.Groups.DagSkabeloner.Commands;
+using TANE.Application.Groups.DagSkabeloner.Commands.Interfaces;
+using TANE.Application.Groups.DagSkabeloner.Queries;
+using TANE.Application.Groups.DagSkabeloner.Queries.Interfaces;
 using TANE.Application.Groups.JwtTokens.Commands;
 using TANE.Application.Groups.JwtTokens.Commands.Interfaces;
 
@@ -16,6 +20,11 @@ namespace TANE.Application.Configuration
             //Registrer application services her
             services.AddScoped<IUserLogin, UserLogin>();
             services.AddScoped<IRefreshToken, RefreshToken>();
+
+            services.AddScoped<ICreateDagSkabelon, CreateDagSkabelon>();
+            services.AddScoped<IUpdateDagSkabelon, UpdateDagSkabelon>();
+            services.AddScoped<IDeleteDagSkabelon, DeleteDagSkabelon>();
+            services.AddScoped<IReadDagSkabelon, ReadDagSkabelon>();
 
             return services;
         }
