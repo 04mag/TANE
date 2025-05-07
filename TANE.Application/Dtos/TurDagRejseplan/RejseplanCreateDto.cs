7474c906b877d3d26669eaf53131793fc28d178e
SupporterSøren <sorenbb97@gmail.com>
2025-05-07 13:35:46 +0200

    ekstra parantes slettet i token hanmdler

--- TANE.Application/Dtos/TurDagRejseplan/RejseplanCreateDto.cs
+++ TANE.Application/Dtos/TurDagRejseplan/RejseplanCreateDto.cs
@@ -1,23 +1,33 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using TANE.Domain.Entities;
 
 namespace TANE.Application.Dtos
 {
-    public class RejseplanCreateDto
-    {
-        public int KundeId { get; set; }
-        public string Titel { get; set; } 
-        public string Beskrivelse { get; set; } 
-        public int AntalVoksne { get; set; }
-        public int AntalBørn { get; set; }
-        public string Lufthavn { get; set; }
-        public double FlyPris { get; set; }
-        public int AntalDage { get; set; }
-        public DateTime AfrejseTidspunkt { get; set; }
-        public bool TilbudSendt { get; set; } = false;
-        public bool Opfølgning { get; set; } = false;
-        public string OpfølgningNote { get; set; }
-        public bool TilbudAccepteret { get; set; } = false;
-        public bool RejseBooket { get; set; } = false;
-        public bool Anulleret { get; set; } = false;
-    }
+	public class RejseplanCreateDto
+	{
+		public int? KundeId { get; set; }
+		public int? AntalVoksne { get; set; }
+		public string? Titel { get; set; }
+		public string? Beskrivelse { get; set; }
+		public int? AntalBørn { get; set; }
+		public string? Lufthavn { get; set; }
+		public double? FlyPris { get; set; }
+		public int? AntalDage { get; set; }
+		public DateTime? AfrejseTidspunkt { get; set; }
+		public string? OpfølgningNote { get; set; }
+		public Status? RejseplanStatus { get; set; } = Status.RejseplanOprettet;
+
+		public enum Status
+		{
+			RejseplanOprettet,
+			TilbudSendt,
+			AfventerOpfølgning,
+			OpfølgningSvaret,
+			TilbudAccepteret,
+			RejseBooket,
+			Annulleret,
+			Udskudt
+		}
+	}
 }
+
