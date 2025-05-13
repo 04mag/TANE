68b37e2fb45060beb4901b25280c92226b92db06
04mag <mathiasgreisen@gmail.com>
2025-05-13 15:51:47 +0200

    Tilføjet metoder i application laget til revoke af tokens

---
+++ TANE.Application/Groups/JwtTokens/Commands/Interfaces/IRevokeToken.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TANE.Application.Groups.JwtTokens.Commands.Interfaces
+{
+    public interface IRevokeToken
+    {
+        Task<bool> RevokeTokenAsync(string email, string jwtToken);
+        Task<bool> RevokeAllTokensAsync(string jwtToken);
+    }
+}
---
+++ TANE.Application/Groups/JwtTokens/Commands/RevokeToken.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TANE.Application.Groups.JwtTokens.Commands.Interfaces;
+using TANE.Application.RepositoryInterfaces;
+
+namespace TANE.Application.Groups.JwtTokens.Commands
+{
+    public class RevokeToken : IRevokeToken
+    {
+        private readonly IJwtTokenRepository jwtTokenRepository;
+
+        public RevokeToken(IJwtTokenRepository jwtTokenRepository)
+        {
+            this.jwtTokenRepository = jwtTokenRepository;
+        }
+
+        public async Task<bool> RevokeAllTokensAsync(string jwtToken)
+        {
+            return await jwtTokenRepository.RevokeAllTokensAsync(jwtToken);
+        }
+
+        public async Task<bool> RevokeTokenAsync(string email, string jwtToken)
+        {
+            return await jwtTokenRepository.RevokeTokenAsync(email, jwtToken);
+        }
+    }
+}
