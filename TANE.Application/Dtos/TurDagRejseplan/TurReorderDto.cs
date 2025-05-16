53a2b95d4ace4edb2e75e4e276732b509063a1e5
SupporterSøren <sorenbb97@gmail.com>
2025-05-16 12:10:54 +0200

    opret i db ved tilføj, samt reorder fungerer i rejseplan edit

--- TANE.Application/Dtos/TurDagRejseplan/TurReorderDto.cs
+++ TANE.Application/Dtos/TurDagRejseplan/TurReorderDto.cs
@@ -5,6 +5,6 @@ namespace TANE.Application.Dtos
     public class TurReorderDto
     {
         [Required] public List<int> TurIds { get; set; }
-        [Required] public byte[] RowVersion { get; set; }
+        [Required] public byte[] OriginalRowVersion { get; set; }
     }
 }
