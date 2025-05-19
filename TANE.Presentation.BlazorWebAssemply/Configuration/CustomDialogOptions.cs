c0e080023311d38c2a71b5be4c58af7b252b8b1a
04mag <mathiasgreisen@gmail.com>
2025-05-19 11:26:54 +0200

    Har skiftet order rækkefølge for skabelon lister til at vise nyeste først. Har også ændret bredde på dialoger til en fast bredde som sættes i CustomDialogOptions

---
+++ TANE.Presentation.BlazorWebAssemply/Configuration/CustomDialogOptions.cs
@@ -0,0 +1,12 @@
+﻿using Radzen;
+
+namespace TANE.Presentation.BlazorWebAssemply.Configuration
+{
+    public static class CustomDialogOptions
+    {
+        public static DialogOptions DialogOptions => new DialogOptions
+        {
+            Width = "60%"
+        };
+    }
+}
